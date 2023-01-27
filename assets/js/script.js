7914d778f414de24add7403ac06f8652edffcbf6
helenahagman <helena@rapibo.se>
2023-01-27 16:08:58 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,44 @@
 //get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = this.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
     
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if(this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit");
-            }else{
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked $(gameType)`);
+                runGame(gameType);
             }
-        })
+        });
         
     }
     
-})
+    runGame("addition"); 
+
+});
 
 /**
  * The main game "loop", called when the script is first loaded 
  * and after the users answers has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
-    //creates two random numbers between 1 and twentyfive
+    //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+
+
+
+    }
 
 }
 
@@ -46,8 +59,12 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1'). textContent = operand1;
+    document.getElementById('operand2'). textContent = operand2;
+    document.getElementById('operator'). textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
