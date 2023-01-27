726192e1cf56c11b84b61f6bfcd9a71465045527
helenahagman <helena@rapibo.se>
2023-01-27 14:56:22 +0000

    Add event listeners for document loading and button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+//wait for the DOM to finish loading before running the game
+//get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = this.getElementsByTagName("button");
+    
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            }else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked $(gameType)`);
+            }
+        })
+        
+    }
+    
+})
+
 function runGame() {
 
 }
@@ -22,7 +42,7 @@ function displayAdditionQuestion() {
 
 }
 
-function displaySubtractionQuestion() {
+function displaySubtractQuestion() {
 
 }
 
@@ -30,6 +50,6 @@ function displayMultiplyQuestion() {
 
 }
 
-function displayDivisionQuestion() {
-    
+function displayDivideQuestion() {
+
 }
