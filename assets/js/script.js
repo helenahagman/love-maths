6287c096b82882a1de99c82e5f137d7a8265acd4
helenahagman <helena@rapibo.se>
2023-01-30 09:18:07 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");    
+    
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
@@ -13,8 +14,14 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
-    
-    runGame("addition"); 
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
+    runGame("addition");
 
 });
 
@@ -23,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the users answers has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
