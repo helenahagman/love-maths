54f85cc0ea8a2a8a76c057d07ab2e0e2ec3e246a
helenahagman <helena@rapibo.se>
2023-01-30 10:43:06 +0000

    Add code to division function

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     runGame("addition");
-
+   
 });
 
 /**
@@ -44,8 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") { 
         displaySubtractQuestion(num1, num2);
-    } else if (gameType === "divide") { 
-        displayDivideQuestion(num1, num2);
+    } else if (gameType === "division") { 
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -92,7 +92,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return [operand1 * operand2, "divide"];
+        return [operand1 / operand2, "division"];
     }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -144,9 +144,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivideQuestion(operand1, operand2) {
+function displayDivisionQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand1').textContent = (operand1 * operand2); 
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 
